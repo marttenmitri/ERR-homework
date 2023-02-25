b7cddac4626a764b17225575b5471a4ebe877749
Martten Mitri <martten@tlu.ee>
2023-02-25 20:45:32 +0200

    Final push
    Added titles and fixes some issues

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ fetch(url)
   .then(response => response.json())
   .then(data => {
     console.log(data.data.category.frontPage);
-
-    for(i = 3; i<=6; i++){
+    
+    for(i = 1; i<=24; i++){
+      if(i !== 2 && i !== 12 && i !== 18){
+    document.getElementById("slider-heading-"+i).innerHTML = data.data.category.frontPage[i].header;
     const sliderContainer = document.getElementById('slider-container-'+i);
     data.data.category.frontPage[i].data.forEach(item => {
       const slide = document.createElement('div');
@@ -23,22 +25,27 @@ fetch(url)
     
     
     new Splide('#slider-'+i, {
+      pagination: false,
       type: 'loop',
-      perPage: 7,
-      perMove: 7,
+      perPage: 6,
+      perMove: 6,
       gap: 20,
+      padding: {
+        left: '5%',
+        right: '5%'
+      },
       breakpoints: {
-        768: {
+        1000: {
           perPage: 4,
           perMove: 4
         },
-        576: {
+        500: {
           perPage: 2,
           perMove: 2
         }
       },
     }).mount();}
-  })
+  }})
   .catch(error => console.error(error));
 
 
